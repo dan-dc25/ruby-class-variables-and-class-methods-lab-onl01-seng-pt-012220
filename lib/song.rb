c63cfc98de4dea409cd6c28fb13a69228d90470a
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2020-02-29 02:20:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,4 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  @@count = o
-  @@artists = []
-  @@genres = []
-  
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @@artists << artist
-    @genre = genre
-    @@genres << genre
-    @@count += 1
-  end
-
 end
